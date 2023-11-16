407eef906ae515981a4bbcb4d0ce5624c21deb72
bigbarin69 <prathik.prejith@gmail.com>
2023-11-16 22:06:34 +0530

    base homepage + welcome animation page

---
+++ src/components/Welcome/index.jsx
@@ -0,0 +1,22 @@
+import "./index.scss";
+
+function Welcome() {
+  return (
+    <>
+      <div className="welcomepage-container">
+        <div className="top-spacer-wel"></div>
+        <div className="base-container-wel">
+          <div className="main-container-wel">
+            <a>Welcome</a>
+            <a>Welcome</a>
+            <a>Welcome</a>
+            <a>Welcome</a>
+            <a>Welcome</a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Welcome;
---
+++ src/pages/MainPage/index.jsx
@@ -0,0 +1,12 @@
+import Welcome from "../../components/Welcome";
+import "./index.scss";
+
+function HomePage() {
+  return (
+    <>
+      <Welcome />
+    </>
+  );
+}
+
+export default HomePage;
