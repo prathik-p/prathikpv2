85dafdfe9bd1a9ce4d87a6e67d447aa93314c0b9
prathik-p <prathik.prejith@gmail.com>
2023-11-16 23:58:54 +0530

    welcome animation

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -7,11 +7,26 @@ function Welcome() {
         <div className="top-spacer-wel"></div>
         <div className="base-container-wel">
           <div className="main-container-wel">
-            <a>Welcome</a>
-            <a>Welcome</a>
-            <a>Welcome</a>
-            <a>Welcome</a>
-            <a>Welcome</a>
+            <a className="w-1">Welcome</a>
+            <a className="w-2">Welcome</a>
+            <a className="w-3">Welcome</a>
+            <a className="w-4">Welcome</a>
+            <a className="w-5">Welcome</a>
+            <a className="w-6">Welcome</a>
+            <a className="w-7">Welcome</a>
+            <a className="w-8">Welcome</a>
+            <a className="w-9">Welcome</a>
+            <a className="w-10">Welcome</a>
+            <a className="w-11">Welcome</a>
+            <a className="w-12">Welcome</a>
+            {/* <a className="w-13">Welcome</a>
+            <a className="w-14">Welcome</a>
+            <a className="w-15">Welcome</a>
+            <a className="w-16">Welcome</a>
+            <a className="w-17">Welcome</a>
+            <a className="w-18">Welcome</a>
+            <a className="w-19">Welcome</a>
+            <a className="w-20">Welcome</a> */}
           </div>
         </div>
       </div>
